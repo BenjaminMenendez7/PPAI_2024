a534d2cddc3aa3b62d6a8dab25a16869ab4be11e
unknown <benjamenendez7@gmail.com>
2024-05-28 22:59:28 -0300

    Primer Commit

---
+++ PPAI_THE_LAST_DANCE/entity/Bodega.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI_THE_LAST_DANCE.entity
+{
+    public class Bodega
+    {
+        private string coordenadasUbicacion;
+        private string descripcion;
+        private string historia;
+        private string nombre;
+        private DateTime periodoActualización;
+
+        
+    }
+}
